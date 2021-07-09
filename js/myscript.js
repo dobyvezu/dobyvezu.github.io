d4b34d1337d2eacad972f0f36c03dbbfe4ba1e55
jakubzvarik <83179616+jakubzvarik@users.noreply.github.com>
2021-07-09 18:55:30 +0200

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+function imgWindow() {
+  window.open("image") }
+
+function smoothScrollProp(){
+    document.querySelector('#propositions').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function smoothScrollInfo(){
+    document.querySelector('#info').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
