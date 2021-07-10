e76dbbd6f27831974b5ea6460026f506394a0522
jakubzvarik <83179616+jakubzvarik@users.noreply.github.com>
2021-07-10 09:04:39 +0200

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -9,7 +9,7 @@ function smoothScrollProp(){
 
 function smoothScrollInfo(id){
   let yOffset = -50;
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+  if( /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) ) {
     yOffset = -700;
 }
   const element = document.getElementById(id);
